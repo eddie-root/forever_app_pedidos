ec0a9081321baa4c9687447985ad85136767b543
eddme-root <ferreiraad3@gmail.com>
2026-08-15 01:15:26 -0400

    changes pasta utils

--- client/src/utils/money.js
+++ client/src/utils/money.js
@@ -1,11 +1,11 @@
-
-export function formatCurrency(priceCents){
-    if (priceCents === undefined) {
-        return (0).toFixed(2)
-    } else {
-        // return (priceCents).toFixed(2)       
-        return (priceCents/100).toFixed(2)       
+export function formatCurrency(valueInCents) {
+    if (valueInCents === undefined || valueInCents === null) {
+        return "R$ 0,00";
     }
+    return new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(valueInCents / 100);
 }
 
 export default formatCurrency;
